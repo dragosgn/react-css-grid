58873c8d1ba4c2345dc451bc6300accc7b8969e2
dragos <dragosgn@gmail.com>
2017-10-19 16:07:41 +0200

    furthher improvements

--- src/App.js
+++ src/App.js
@@ -3,58 +3,71 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   max-width: 1140px;
-  background-color: blue;
+  width: 100%;
   margin-right: auto;
   margin-left: auto;
-`
-
-const Grid = styled.div`
-  width: 100%;
-
+  box-sizing: border-box;
 `
 
 const Col = styled.div`
   display: flex;
   flex-direction: column;
-  background-color: red;
+  float:left;
+  min-height: 5px;
   width: calc(100% * (${props => props.col} / 12));
-  margin: 0 1rem 0 1rem;
+  padding: 0.5rem;
 `
 
 const Row = styled.div`
   display: flex;
   flex-direction: row;
-  background-color: yellow;
-  margin: 0.5rem 0 0.5rem 0;
+  /*-- our cleafix hack -- */
+  .row:before,
+    .row:after {
+        content:"";
+        display: table ;
+        clear:both;
+    }
 `
 
 
 
 const PerfHeader = styled(Row)`
-
+  background-color: #253040;
+  color: white;
+  border-radius:
 `
 
 const PerfBody = styled(Row)`
-  min-height: 200px;
+  min-height: 300px;
+  background-color: white;
 `
 
-const PerfFooter = styled(Row)``
+const PerfFooter = styled(Row)`
+  background-color: #253040;
+  border-radius: 0 0 5px 5px;
+`
+const PerfRoot = styled(Col)`
+`
 
 const PerformanceComponent = ({title, content}) => {
   return(
-    <Col col={12}>
+    <PerfRoot col={12}>
       <PerfHeader>
-        <Col col={4}>{title}</Col>
-        <Col col={4}></Col>
-        <Col col={4}>Date</Col>
+        <Col col={7}>{title}</Col>
+        <Col col={5}>26.04.2013 - 19.10.2017</Col>
       </PerfHeader>
       <PerfBody>
         <Col col={12}>
           {content}
         </Col>
       </PerfBody>
-      <PerfFooter></PerfFooter>
-    </Col>
+      <PerfFooter>
+        <Col>
+
+        </Col>
+      </PerfFooter>
+    </PerfRoot>
   )
 }
 
@@ -65,26 +78,30 @@ class App extends Component {
         <Row>
           <Col col={12}>
             <Row>
-              <h1>LEISTUNGSÜBERSICHT</h1>
+              <h1 style={{color: "#253040"}}>LEISTUNGSÜBERSICHT</h1>
             </Row>
             <Row>
               <Col col={12}>
-                <Row>
+                <Row style={{backgroundColor: "#253040", color: "white"}}>
                   <Col col={1}><p>Sanitär</p></Col>
-                  <Col col={11}><p>Date</p></Col>
+                  <Col col={11}><p>26.04.2013 - 19.10.2017</p></Col>
                 </Row>
                 <Row>
-                  <Col col={12}>
+                  <Col col={12} style={{backgroundColor: "#D3D3D3"}}>
                     <Row>
                       <h1>SERVICECENTER</h1>
                     </Row>
                     <Row>
-                      <Col col={6}>
-
-                      </Col>
-                      <Col col={6}>
-
-                      </Col>
+                      <PerformanceComponent title={"Ihr Servicestatus"} content={"Clock"} />
+                      <PerformanceComponent title ={"Neueste unbearbeitete Anfragen"} content={"Table"} />
+                    </Row>
+                    <Row>
+                      <PerformanceComponent title={"Anfragenverteilung nach Status"} content={"Clock"} />
+                      <PerformanceComponent title ={"Neueste unbearbeitete Anfragen"} content={"Table"} />
+                    </Row>
+                    <Row>
+                      <PerformanceComponent title={"Ihr Servicestatus"} content={"Clock"} />
+                      <PerformanceComponent title ={"Neueste unbearbeitete Anfragen"} content={"Table"} />
                     </Row>
                   </Col>
                 </Row>
